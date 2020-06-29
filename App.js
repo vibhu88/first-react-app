ff5934054a5e1cdb56b5ed317fa260050ff0fdff
vibhu88 <vibhu.eclipse@gmail.com>
2020-06-30 02:46:27 +0530

    Updated App to latest structure - Lesson 15 Todo List complete

--- App.js
+++ App.js
@@ -6,9 +6,9 @@ import AddTodo from './components/addTodo';
 
 export default function App() {
   const [todos, setTodos] = useState([
-    { text: 'Arrange Warranty', key: '1'},
-    { text: 'Wash Utensils', key: '2'},
-    { text: 'Mop Floor', key: '3'},
+    { text: 'buy coffee', key: '1' },
+    { text: 'create an app', key: '2' },
+    { text: 'play on the switch', key: '3' },
   ]);
 
   const pressHandler = (key) => {
@@ -16,10 +16,9 @@ export default function App() {
       return prevTodos.filter(todo => todo.key != key);
     });
   };
-  
+
   const submitHandler = (text) => {
     if(text.length > 3){
-      setText('');
       setTodos(prevTodos => {
         return [
           { text, key: Math.random().toString() },
@@ -34,7 +33,10 @@ export default function App() {
   };
 
   return (
-    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+      console.log('dismissed');
+    }}>
       <View style={styles.container}>
         <Header />
         <View style={styles.content}>
@@ -60,8 +62,10 @@ const styles = StyleSheet.create({
   },
   content: {
     padding: 40,
+    flex: 1,
   },
   list: {
     marginTop: 20,
+    flex: 1,
   },
 });
